cecec6aefb666e19ca07781504285c8aaca365ad
Yvonne Tsu <yvonnnetsu@gmail.com>
2017-03-10 15:50:52 -0800

    login and logout and flash messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,8 @@ class UsersController < ApplicationController
 
   def create
     @users = User.create(user_params)
-    redirect_to root_path
+    login(@user)
+    redirect_to @user
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root to: 'users#index'
 
-  get '/users/new', to: 'users#new', as: 'new_user'
-  post '/users', to: 'users#create'
+  get   '/users/new', to: 'users#new',      as: 'new_user'
+  post  '/users',     to: 'users#create'
+  get   '/users/:id', to: 'users#show',     as: 'user'
+  get   '/login',     to: 'sessions#new'
+  get   '/logout',    to: 'sessions#destroy'
+  post  '/sessions',  to: 'sessions#create'
 end
