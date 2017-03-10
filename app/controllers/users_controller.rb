3be7a68be8d38cd42890f555b25b6d69bec4f3a2
Yvonne Tsu <yvonnnetsu@gmail.com>
2017-03-10 14:35:53 -0800

    added new + create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,19 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @users = User.create(user_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password)
+  end
+
 end
