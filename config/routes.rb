77ca602b8fbcf51d415dd169917f14973e9eb8dc
Yvonne Tsu <yvonnnetsu@gmail.com>
2017-03-11 09:18:55 -0800

    added library model + routes + updated user and library relationship

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   root to: 'users#index'
 
-  get   '/users/new', to: 'users#new',      as: 'new_user'
-  post  '/users',     to: 'users#create'
-  get   '/users/:id', to: 'users#show',     as: 'user'
-  get   '/login',     to: 'sessions#new'
-  get   '/logout',    to: 'sessions#destroy'
-  post  '/sessions',  to: 'sessions#create'
+  get   '/users/new',      to: 'users#new',       as: 'new_user'
+  post  '/users',          to: 'users#create'
+  get   '/users/:id',      to: 'users#show',      as: 'user'
+  get   '/login',          to: 'sessions#new'
+  get   '/logout',         to: 'sessions#destroy'
+  post  '/sessions',       to: 'sessions#create'
+
+  get   '/libraries',      to: 'libraries#index'
+  get   '/libraries/new',  to: 'libraries#new',   as: 'new_library'
+  post  '/libraries',      to: 'libraries#create'
 end
