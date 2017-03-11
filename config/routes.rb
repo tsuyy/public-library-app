3b9e1c20aa613a7f495adee96b0cc47f8db672ba
Yvonne Tsu <yvonnnetsu@gmail.com>
2017-03-11 12:27:52 -0800

    initial styling commit

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,8 @@ Rails.application.routes.draw do
   get   '/libraries',      to: 'libraries#index'
   get   '/libraries/new',  to: 'libraries#new',   as: 'new_library'
   post  '/libraries',      to: 'libraries#create'
+
+  get '/users/:user_id/libraries',     to: 'library_users#index',  as: 'user_libraries'
+  post '/libraries/:library_id/users', to: 'library_users#create', as: 'library_users'
+
 end
